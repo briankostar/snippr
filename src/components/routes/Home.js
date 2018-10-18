56ecc164759bab07feec72ff22fe3743ee793c43
Brian Ko <brian.kostar@gmail.com>
2018-10-17 23:15:39 -0400

    sort by votes

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -4,14 +4,13 @@ import Lists from '../views/Lists'
 class Home extends Component {
     constructor(props) {
         super(props)
-        this.tempData = [{ id: 1, votes: 1, link: 'test link 1' }, { id: 2, votes: 2, link: 'test link 2' }, { id: 3, votes: 3, link: 'test link 3' }];
+        this.tempData = [{ id: 1, votes: 1560, link: 'Awesome List' }, { id: 2, votes: 2014, link: 'React' }, { id: 3, votes: 302, link: 'Scary Movies' }];
     }
     render() {
         return (
             <div>
                 <h1>Snipper</h1>
                 <Lists lists={this.tempData}></Lists>
-                {/* <div>{this.tempData.map((list) => <List key={list.id} {...list} />)}</div> */}
             </div>
         )
     }
--- src/components/views/Lists.js
+++ src/components/views/Lists.js
@@ -6,7 +6,7 @@ class Lists extends Component {
     render() {
         return (
             <div>
-                <div>{this.props.lists.map((list) => <List key={list.id} {...list} />)}</div>
+                <div>{this.props.lists.sort((a, b) => b.votes - a.votes).map((list) => <List key={list.id} {...list} />)}</div>
             </div>
         )
     }
