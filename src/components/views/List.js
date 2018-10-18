e4c9c75890a7c68d14b9617f37d9d6a44705975b
Brian Ko <brian.kostar@gmail.com>
2018-10-17 23:27:43 -0400

    add bulma component

--- src/components/views/List.js
+++ src/components/views/List.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Columns } from 'react-bulma-components';
 
 class List extends Component {
 
     render() {
         return (
             <div>
-                Votes: {this.props.votes}
-                <Link to={`/post/${this.props.id}`} className="navbar-item">
-                    {this.props.link}
-                </Link>
+                <Columns.Column>
+                    {this.props.votes}
+                    <Link to={`/post/${this.props.id}`} className="navbar-item">
+                        {this.props.link}
+                    </Link>
+                </Columns.Column>
             </div>
         )
     }
--- src/components/views/Lists.js
+++ src/components/views/Lists.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import List from './List'
+import { Columns } from 'react-bulma-components';
 
 class Lists extends Component {
 
     render() {
         return (
             <div>
-                <div>{this.props.lists.sort((a, b) => b.votes - a.votes).map((list) => <List key={list.id} {...list} />)}</div>
+                <Columns>{this.props.lists.sort((a, b) => b.votes - a.votes).map((list) => <List key={list.id} {...list} />)}</Columns>
             </div>
         )
     }
