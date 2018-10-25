3688c33c2f3d64f293d96dfca8dc7df0b7bf149d
Brian Ko <brian.kostar@gmail.com>
2018-10-25 11:59:51 -0400

    add nav

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/routes/Home'
 import PostRoute from './components/routes/PostRoute'
 
+import Navbar from './components/views/Navbar'
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
+          <Navbar></Navbar>
           <Route exact={true} path='/' render={(props) => <Home {...props} title={'Home'} />} />
           <Route path='/post/:id' render={(props) => <PostRoute {...props} title={'Post'} />} />
         </div>
