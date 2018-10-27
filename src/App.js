c558307220e3907385a5412fd0a0752fe841f29b
Brian Ko <brian.kostar@gmail.com>
2018-10-26 22:37:11 -0400

    add okta

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { SecureRoute, ImplicitCallback } from '@okta/okta-react';
 
 import Home from './components/routes/Home'
 import PostRoute from './components/routes/PostRoute'
 import Signup from './components/routes/Signup'
-
 import Navbar from './components/views/Navbar'
+
+import config from './app.config';
+import Login from './components/routes/Login'
+import Profile from './components/routes/Profile'
+import SignupForm from './components/views/SignupForm'
 
 class App extends Component {
   render() {
@@ -15,7 +20,12 @@ class App extends Component {
           <Navbar></Navbar>
           <Route exact={true} path='/' render={(props) => <Home {...props} title={'Home'} />} />
           <Route path='/post/:id' render={(props) => <PostRoute {...props} title={'Post'} />} />
-          <Route path='/signup' render={(props) => <Signup {...props} title={'Signup'} />} />
+          {/* <Route path='/signup' render={(props) => <Signup {...props} title={'Signup'} />} /> */}
+
+          <Route path="/login" render={() => <Login baseUrl={config.url} />} />
+          <Route path="/implicit/callback" component={ImplicitCallback} />
+          <Route path="/signup" component={SignupForm} />
+          <SecureRoute path="/profile" component={Profile} />
         </div>
       </BrowserRouter>
     );
--- src/components/views/Navbar.js
+++ src/components/views/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { withAuth } from '@okta/okta-react';
 
 class Navbar extends Component {
 
